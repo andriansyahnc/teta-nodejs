110f77a5b7a6f7619646fb346f94ecb7d9fec3fe
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2023-03-11 14:29:13 +0700

    refactor: base connector multiple export

--- src/shared/databases/mongoose/Connector.ts
+++ src/shared/databases/mongoose/Connector.ts
@@ -1,6 +1,6 @@
 import * as mongoose from "mongoose";
 import config from "../../config";
-import { BaseConnector } from "../BaseConnector";
+import BaseConnector from "../BaseConnector";
 
 class Connector extends BaseConnector {
 
