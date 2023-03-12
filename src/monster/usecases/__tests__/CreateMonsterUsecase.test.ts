77511e70b962c1ed09376adead3951f8e3286946
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2023-03-12 14:28:16 +0700

    add monster unit tests

--- src/monster/usecases/__tests__/CreateMonsterUsecase.test.ts
+++ src/monster/usecases/__tests__/CreateMonsterUsecase.test.ts
@@ -48,4 +48,25 @@ describe("CreateMonsterUsecase", () => {
         };
         await expect(usecase.execute(invalidMonsterData)).rejects.toThrow();
     });
+
+    it("should throw an error when something is happened in the database", async () => {
+        const monsterData = {
+            "name": "Bulbasaur",
+            "nickname": "The little Bulba",
+            "types": [
+                "Grass",
+                "Poison"
+            ],
+            "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+            "description": "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+            "height": "2' 04\"",
+            "weight": "15.2 lbs",
+            "hp": 30,
+            "attack": 30,
+            "defense": 30,
+            "speed": 30
+        };
+        await mongoServer.stop();
+        await expect(usecase.execute(monsterData)).rejects.toThrow();
+    });
 });
